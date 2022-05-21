cce336a71d5394c6e22ffbe063a2cdd84ae1089b
thapaRoyal <thaparoyal17@gmail.com>
2022-05-21 13:59:26 +0545

    HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input type="text" className="inputBox" placeholder="Room ID" />
+          <input type="text" className="inputBox" placeholder="USERNAME" />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built with 💛️ by{' '}
+          <a href="https://github.com/thapaRoyal" target="_blank">
+            thapaRoyal
+          </a>
+        </h4>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
