c9ff854a56b44ffcda364b6922b36ab7248c74d8
thapaRoyal <thaparoyal17@gmail.com>
2022-05-24 21:33:58 +0545

    create avatar

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Avatar from 'react-avatar';
 
-const Client = (username) => {
+const Client = ({ username }) => {
   return (
     <div className="client">
-      <Avatar />
+      <Avatar name={username} size={50} round="14px" />
       <span className="userName">{username}</span>
     </div>
   );
