67f3593cf9de9550b6a97e07cd70d8101f851a64
thapaRoyal <thaparoyal17@gmail.com>
2022-05-31 09:23:37 +0545

    server

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,14 +1,29 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+// import ACTIONS from '../../Actions';
 import Client from '../components/Client';
 import Editor from '../components/Editor';
+import { initSocket } from '../socket';
 
 const EditorPage = () => {
   const socketRef = useRef(null);
-  useEffect(() => {}, []);
+  const location = useLocation();
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+    };
+    init();
+  }, []);
+
   const [clients, setClients] = useState([
     { socketId: 1, username: 'royal' },
     { socketId: 2, username: 'john doe' },
   ]);
+
   return (
     <div className="mainWrap">
       <div className="aside">
